c0e4a2eb70b5b864e554e7a3e5ac9012945641fd
Volodymyr Solonenko <volodymir.solonenko@gmail.com>
2019-10-09 00:50:23 +0300

    Improve AppProperties: add defaults

--- agent/src/main/java/com/modata/test/agent/helpers/AppProperties.java
+++ agent/src/main/java/com/modata/test/agent/helpers/AppProperties.java
@@ -5,22 +5,35 @@ import com.modata.test.agent.Application;
 import java.io.IOException;
 import java.util.Properties;
 
+import static java.lang.String.format;
+
 public class AppProperties {
-    static Properties properties = new Properties();
+    private static final String PROPERTIES_FILE_NAME = "config.properties";
+    private static Properties PROPERTIES = new Properties() {{
+        defaults = new Properties();
+        defaults.put(Names.AGENCY_URL, "http://localhost:8080");
+        defaults.put(Names.PUSH_INTERVAL, "60000");
+    }};
+
     static {
         try {
-            properties.load(Application.class.getClassLoader().getResourceAsStream("config.properties"));
-        }
-        catch (IOException ex) {
-            ex.printStackTrace();
+            PROPERTIES.load(Application.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME));
+        } catch (IOException e) {
+            throw new RuntimeException(format("FATAL ERROR: Failed to read application PROPERTIES from '%s'.", PROPERTIES_FILE_NAME), e);
         }
     }
 
     public static String getAgencyUrl() {
-        return properties.getProperty("agency.url");
+        return PROPERTIES.getProperty(Names.AGENCY_URL);
     }
 
     public static Integer getInterval() {
-        return Integer.parseInt(properties.getProperty("push.interval"));
+        return Integer.parseInt(PROPERTIES.getProperty(Names.PUSH_INTERVAL));
     }
+
+    private static class Names {
+        static final String AGENCY_URL = "agency.url";
+        static final String PUSH_INTERVAL = "push.interval";
+    }
+
 }
