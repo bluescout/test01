6fec8a9fea5fdfac07706f23a80c1233fba16c57
Volodymyr Solonenko <volodymir.solonenko@gmail.com>
2019-10-21 01:28:07 +0300

    Make API classes package-private

--- agency/src/main/java/test/agency/controllers/AgentApi.java
+++ agency/src/main/java/test/agency/controllers/AgentApi.java
@@ -1,18 +1,18 @@
 package test.agency.controllers;
 
-public final class AgentApi {
+final class AgentApi {
     private AgentApi() {
     }
 
     private static final String BASE_PATH = "/agent-api";
 
-    public static class V1 {
+    final static class V1 {
         private V1() {
         }
 
         private static final String BASE_PATH_V1 = BASE_PATH + "/v1";
 
-        public static final String METRICS = BASE_PATH_V1 + "/computers/{computerId}/metrics";
+        static final String METRICS = BASE_PATH_V1 + "/computers/{computerId}/metrics";
     }
 
 }
--- agency/src/main/java/test/agency/controllers/UiApi.java
+++ agency/src/main/java/test/agency/controllers/UiApi.java
@@ -1,20 +1,20 @@
 package test.agency.controllers;
 
-public final class UiApi {
+final class UiApi {
     private UiApi() {
     }
 
     private static final String BASE_PATH = "/ui-api";
 
-    public static class V1 {
+    final static class V1 {
         private V1() {
         }
 
         private static final String BASE_PATH_V1 = BASE_PATH + "/v1";
 
-        public static final String COMPUTERS = BASE_PATH_V1 + "/computers";
-        public static final String METRICS = COMPUTERS + "/{computerId}";
-        public static final String METRICS_HISTORY = COMPUTERS + "/{computerId}/history";
+        static final String COMPUTERS = BASE_PATH_V1 + "/computers";
+        static final String METRICS = COMPUTERS + "/{computerId}";
+        static final String METRICS_HISTORY = COMPUTERS + "/{computerId}/history";
     }
 
 }
